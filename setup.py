2e850530a463d4df8a3325232e579b881d6987bf
Monte C Roden <monte.roden@wsu.edu>
2017-11-01 14:50:42 -0700

    Put v0.1 on PyPI

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(name='spoilerfy',
       version='0.1',
-      description="""Put 4 spaces in front of each line, then
-                    copy those lines to your clipboard.""",
+      description='Put 4 spaces in front of each line, then copy those lines to your clipboard.',
       url='https://www.github.com/rodenmonte/spoilerfy',
       author='Monte Roden',
       author_email='rodenmonte@gmail.com',
       license='MIT',
       packages=['spoilerfy'],
+      install_requires=[
+        'pyperclip'
+      ],
       zip_safe=False)
